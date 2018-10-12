c8e20503ebdcbf6cbc8116a318f63a191651f8e6
Daniel Khan <daniel.khan@dynatrace.com>
2018-10-12 23:15:38 +0200

    New Deployment

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const expressStatsd = require('express-statsd');
+const appstatsd = require('appmetrics-statsd').StatsD();
 
 const indexRouter = require('./routes/index');
 
