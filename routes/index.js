fb27279bd555ccf0a9a6f8ddad5a37f39ec07aa9
Daniel Khan <daniel.khan@dynatrace.com>
2018-10-13 22:14:09 +0200

    New Deployment

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 /* GET home page. */
 router.get('/', async (req, res, next) => {
 
-  const httpres = axios.get('https://us-central1-sai-research.cloudfunctions.net/http');
+  const httpres = await axios.get('https://us-central1-sai-research.cloudfunctions.net/http');
   console.log(httpres.response.data);
   res.render('index', { title: 'Express' });
 });
