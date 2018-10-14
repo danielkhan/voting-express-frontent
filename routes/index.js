bf24ffadc3572ea25a17fe5c410c52a08d2f7f9e
Daniel Khan <daniel.khan@dynatrace.com>
2018-10-14 11:39:13 +0200

    New Deployment

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,13 @@ const axios = require('axios');
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
+  try {
+    const httpres = await axios.get('http://localhost:3001');
+    res.render('index', { title: 'Express' });
+  } catch(err) {
+    return next(err);
+  }
 
-  const httpres = await axios.get('http://localhost:3001');
-  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
