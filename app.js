6dc5fcb4e44362a241a9714806a2ea719b5708ac
Daniel Khan <daniel.khan@dynatrace.com>
2018-10-15 17:35:14 +0200

    New Deployment

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-const appzip = require('appmetrics-zipkin')({
-  host: 'localhost',
-  port: 9411,
-  serviceName:'express-frontend',
-  sampleRate: 1.0
-});
-
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
