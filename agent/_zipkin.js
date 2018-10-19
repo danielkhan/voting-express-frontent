b125f07e8570fda63226748746769dcdec24933f
Daniel Khan <daniel.khan@dynatrace.com>
2018-10-19 17:25:55 -0300

    New Deployment

--- agent/_zipkin.js
+++ agent/_zipkin.js
@@ -9,31 +9,28 @@ const zipkinMiddleware = require('zipkin-instrumentation-express').expressMiddle
 const wrapRequest = require('zipkin-instrumentation-request');
 const request = require('request');
 const ctxImpl = new ExplicitContext();
-// const recorder = new ConsoleRecorder();
-const localServiceName = 'express-frontend'; // name of this application
-// const tracer = new Tracer({ ctxImpl, recorder, localServiceName });
-
+const localServiceName = 'express-frontend'; 
 
 // const CLSContext = require('zipkin-context-cls');
 const { HttpLogger } = require('zipkin-transport-http');
 
 // Setup the tracer to use http and implicit trace context
-
+const tracer = new Tracer({
+  ctxImpl,
+  recorder: new BatchRecorder({
+    logger: new HttpLogger({
+      endpoint: 'http://localhost:9411/api/v2/spans',
+      jsonEncoder: JSON_V2
+    })
+  }),
+  localServiceName,
+});
 
 
 
 module.exports = (localServiceName) => {
 
-  const tracer = new Tracer({
-    ctxImpl,
-    recorder: new BatchRecorder({
-      logger: new HttpLogger({
-        endpoint: 'http://localhost:9411/api/v2/spans',
-        jsonEncoder: JSON_V2
-      })
-    }),
-    localServiceName,
-  });
+
 
   return {
     middleware: () => zipkinMiddleware({ tracer }),
--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const {
 const zipkinMiddleware = require('zipkin-instrumentation-express').expressMiddleware;
 
 const ctxImpl = new ExplicitContext();
+const CLSContext = require('zipkin-context-cls');
 const localServiceName = 'express-frontend';
 
 // const CLSContext = require('zipkin-context-cls');
@@ -15,7 +16,7 @@ const { HttpLogger } = require('zipkin-transport-http');
 
 // Setup the tracer to use http and implicit trace context
 const tracer = new Tracer({
-  ctxImpl,
+  ctxImpl: new CLSContext('zipkin'),
   recorder: new BatchRecorder({
     logger: new HttpLogger({
       endpoint: 'http://localhost:9411/api/v2/spans',
