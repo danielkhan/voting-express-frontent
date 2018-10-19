c1170674792650e72c22c7dc78571e0f1b2a2833
Daniel Khan <daniel.khan@dynatrace.com>
2018-10-19 16:53:50 -0300

    New Deployment

--- agent/zipkin.js
+++ agent/zipkin.js
@@ -37,6 +37,6 @@ module.exports = (localServiceName) => {
 
   return {
     middleware: () => zipkinMiddleware({ tracer }),
-    request: (remoteServiceName) => wrapRequest(request, { tracer }),
+    request: (remoteServiceName) => wrapRequest(request, { tracer, remoteServiceName }),
   }
 } 
