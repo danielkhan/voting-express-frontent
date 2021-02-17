abccd5529845740e927d1124383a1e112522ab2b
Daniel Khan <daniel@khan.io>
2021-02-17 01:23:49 +0100

    debugging

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const axios = require('axios');
-const request = require('request');
+const axios = require("axios");
+const request = require("request");
 
 const votes = {
   spaces: [],
@@ -11,38 +11,39 @@ const votes = {
 const visits = [];
 
 module.exports = () => {
-
-  router.get('/', async (req, res, next) => {
+  router.get("/", async (req, res, next) => {
+    console.log("GET ROUTE");
     try {
-
       // const str = new Array(100000).join('*');
-  
 
-      if (req.query.choice && req.query.choice !== 'spaces' && req.query.choice !== 'tabs') {
+      if (
+        req.query.choice &&
+        req.query.choice !== "spaces" &&
+        req.query.choice !== "tabs"
+      ) {
         return res.status(400).end();
       }
-      const httpres = request.get(`http://localhost:3001?choice=${req.query.choice}`, (e, r) => {
-        if (e) return next(e);
-        return res.render('index', JSON.parse(r.body));
-      });
-      
+      const httpres = request.get(
+        `http://localhost:3001?choice=${req.query.choice}`,
+        (e, r) => {
+          if (e) return next(e);
+          return res.render("index", JSON.parse(r.body));
+        }
+      );
     } catch (err) {
       return next(err);
     }
   });
 
-
-  router.get('/traces', (req, res, next) => {
-    const host = req.headers['host'];
+  router.get("/traces", (req, res, next) => {
+    const host = req.headers["host"];
     return res.redirect(`http://${host}:16686`);
   });
 
-  router.get('/metrics', (req, res, next) => {
-    const host = req.headers['host'];
+  router.get("/metrics", (req, res, next) => {
+    const host = req.headers["host"];
     return res.redirect(`http://${host}:3000`);
   });
 
-
   return router;
-}
-
+};
