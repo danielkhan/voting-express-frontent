32f25e726596590de22211e089367ba533b9ed61
Daniel Khan <daniel@khan.io>
2021-02-17 01:03:09 +0100

    update tracing

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-const tracing = require("./agent/tracing");
-tracing.init("frontend");
-
-const tracer = tracing.getTracer("frontend");
-tracing.enableConsoleExporter();
-tracing.enableJaegerExporter();
-
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
