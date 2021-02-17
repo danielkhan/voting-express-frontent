3c5b105b101f7a59a650a7a5ad27044627162dc7
Daniel Khan <daniel@khan.io>
2021-02-17 02:58:05 +0100

    fix tracer init

--- agent/tracing.js
+++ agent/tracing.js
@@ -16,20 +16,19 @@ const provider = new NodeTracerProvider();
 
 module.exports.init = (serviceName) => {
   _serviceName = serviceName;
+  provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+
+  // Enable jaeger exporter
+  const options = {
+    serviceName: _serviceName,
+    host: "localhost",
+    port: 6832,
+  };
+  const jaegerExporter = new JaegerExporter(options);
+  provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter));
+  provider.register();
 };
 
 module.exports.getTracer = (name) => {
   return opentelemetry.trace.getTracer(name);
 };
-
-provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
-
-// Enable jaeger exporter
-const options = {
-  serviceName: _serviceName,
-  host: "localhost",
-  port: 6832,
-};
-const jaegerExporter = new JaegerExporter(options);
-provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter));
-provider.register();
